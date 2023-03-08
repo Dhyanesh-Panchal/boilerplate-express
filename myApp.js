55bb4c484fb55d48df1bc7b4b649b2c23109b1f7
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-03-09 01:28:27 +0530

    started with express module

--- myApp.js
+++ myApp.js
@@ -1,5 +1,23 @@
 let express = require('express');
 let app = express();
+require('dotenv').config() // to load enviornment variables from .env file
+
+
+app.use('/public', express.static(__dirname + '/public'))
+// console.log('Hello World')
+app.get('/', (req, res) => {
+    // res.send("Hello Express");
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        res.json({ "message": "Hello json".toUpperCase() });
+    }
+    else {
+        res.json({ "message": "Hello json" });
+    }
+})
 
 
 
@@ -32,8 +50,4 @@ let app = express();
 
 
 
-
-
-
-
- module.exports = app;
+module.exports = app;
