8d7563a62c20fdbdf5c81df3d0e7b1e171ac401a
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-03-10 00:37:11 +0530

    Completed express Module

--- myApp.js
+++ myApp.js
@@ -1,13 +1,21 @@
 let express = require('express');
 let app = express();
+const bodyParser = require('body-parser')
 require('dotenv').config() // to load enviornment variables from .env file
 
 
 app.use('/public', express.static(__dirname + '/public'))
 // console.log('Hello World')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
 app.get('/', (req, res) => {
     // res.send("Hello Express");
     res.sendFile(__dirname + '/views/index.html');
+    // console.log(req)
 })
 
 app.get('/json', (req, res) => {
@@ -19,6 +27,28 @@ app.get('/json', (req, res) => {
     }
 })
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({ time: req.time });
+})
+
+app.get('/:word/echo', (req, res) => {
+    let echoWord = { echo: req.params.word }
+    res.json(echoWord);
+})
+
+app.get('/name', (req, res) => {
+    let reqName = `${req.query.first} ${req.query.last}`;
+    console.log(reqName);
+    res.json({ name: reqName });
+})
+
+app.post('/name', (req, res) => {
+    let Name = `${req.body.first} ${req.body.last}`;
+    res.json({ name: Name })
+})
 
 
 
